c248a554fe25111717c711c5b3b37ff02ff0d8c3
Mike-Vidal <113799147+Mike-Vidal@users.noreply.github.com>
2023-10-05 00:03:23 -0400

    Finalized styles and event listeners

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const pauseButton = document.querySelector("#pause");
 const stopButton = document.querySelector("#stop");
 
 const timeDisplay = document.querySelector(".time-display");
+const phaseDisplay = document.querySelector(".phase-display");
 
 // UI Functions
 
@@ -35,22 +36,76 @@ const updateTimeDisplay = () => {
   })}`;
 };
 
+const updatePhaseDisplay = () => {
+  phaseDisplay.innerText = phaseTitle;
+};
+
 startButton.addEventListener("click", () => {
+  addActiveStyle(startButton);
   timerID = setInterval(() => {
     time -= 1;
     // Zero Occurence
-
+    if (time === 0) {
+      clearInterval(timerID);
+      handlePhaseChange();
+      updatePhaseDisplay();
+    }
     updateTimeDisplay();
-  }, 1000);
+  }, 10);
 });
 
 pauseButton.addEventListener("click", () => {
+  addActiveStyle(pauseButton);
   clearInterval(timerID);
 });
 
 stopButton.addEventListener("click", () => {
+  addActiveStyle(stopButton);
   clearInterval(timerID);
   time = PHASE_TIME;
-  //   timeDisplay.innerText = timeText;
   updateTimeDisplay();
 });
+
+const updatePhase = (newPhaseID, newTime, newTitle) => {
+  time = newTime;
+  PHASE_TIME = newTime;
+  phaseID = newPhaseID;
+  phaseTitle = newTitle;
+};
+
+const handlePhaseChange = () => {
+  switch (phaseID) {
+    case 1:
+      updatePhase(2, 300, "Break");
+      break;
+    case 2:
+      updatePhase(3, 1500, "Work");
+      break;
+    case 3:
+      updatePhase(4, 300, "Break");
+      break;
+    case 4:
+      updatePhase(5, 1500, "Work");
+      break;
+    case 5:
+      updatePhase(6, 300, "Break");
+      break;
+    case 6:
+      updatePhase(7, 1500, "Work");
+      break;
+    case 7:
+      updatePhase(8, 1800, "Break");
+      break;
+    case 8:
+      updatePhase(1, 1500, "Work");
+      break;
+  }
+};
+
+const addActiveStyle = (buttonToUpdate) => {
+  const buttons = [startButton, pauseButton, startButton];
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+  buttonToUpdate.classList.add("active");
+};
