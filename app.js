94ff1722bf31fbd0a13be26cdb43007f7273a5df
Mike-Vidal <113799147+Mike-Vidal@users.noreply.github.com>
2023-10-04 21:28:35 -0400

    Implemented Start event listerner

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-alert("Hello World");
+// Work 25
+// Rest 5
+// Work 25
+// Rest 5
+// Work 25
+// Rest 5
+// Work 25
+// Rest 30
+
+// Variable Value Definations
+
+let timeText = "25:00";
+// TIME IN SECONDS
+let time = 1500;
+let timerID = null;
+
+// DOM Element Definations
+const startButton = document.querySelector("#start");
+
+const timeDisplay = document.querySelector(".time-display");
+
+// UI Functions
+startButton.addEventListener("click", () => {
+  timerID = setInterval(() => {
+    time -= 1;
+    // Zero Occurence
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    timeDisplay.innerText = `${minutes}:${seconds.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+    })}`;
+    console.log(minutes, seconds);
+  }, 100);
+});
